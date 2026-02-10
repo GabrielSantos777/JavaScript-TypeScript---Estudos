16b027107c794ac4330d40a9fe8d1ecb50d795a6
Gabriel Santos <gabrielsantos.erick12@gmail.com>
2026-02-10 11:48:50 -0300

    Aula e aprendizado sobre operadores de comparação. Já tinha iniciado esse curso um tempo atrás, estou revenedo algumas aulas e continuar de onde eu parei

---
+++ secao_03_logica_de_programacao/Operadores de Comparacao/index.js
@@ -0,0 +1,37 @@
+/* Operadores de Comparação */
+
+// Igualdade (==)
+console.log(5 == '5'); // true
+
+// Estritamente Igual (===)
+console.log(5 === '5'); // false
+
+// Desigualdade (!=)
+console.log(5 != '5'); // false
+
+// Estritamente Desigual (!==)
+console.log(5 !== '5'); // true
+
+// Maior que (>)
+console.log(10 > 5); // true
+
+// Menor que (<)
+console.log(5 < 10); // true
+
+// Maior ou igual (>=)
+console.log(10 >= 10); // true
+
+// Menor ou igual (<=)
+console.log(5 <= 10); // true
+
+// Comparação entre variáveis
+let a = 20;
+let b = '20';
+console.log(a == b); // true
+console.log(a === b); // false
+console.log(a != b); // false
+console.log(a !== b); // true
+console.log(a > 15); // true
+console.log(b < 25); // true
+console.log(a >= 20); // true
+console.log(b <= 20); // true
