559674b56cc95b89b98ff99bb5a43f1a230fb4cf
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-03-30 14:23:53 -0300

    aula sobre ajax, criação de um httprequest utilizando promises

---
+++ secao_08_JS_Assincrono/a04_xmlhttprequest_promises/assets/js/main.js
@@ -0,0 +1,96 @@
+const httpRequest = (obj) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
+
+    xhr.addEventListener("load", () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    });
+  });
+};
+
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if (tag === "a") {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
+
+async function carregaPagina(el) {
+  const href = el.getAttribute("href");
+
+  const objConfig = {
+    method: "GET",
+    url: href,
+  };
+
+  try {
+    const response = await httpRequest(objConfig);
+    carregaResultado(response);
+  } catch (error) {
+    console.log(error);
+  }
+
+  
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector(".resultado");
+  resultado.innerHTML = response;
+}
+
+// METÓDO ANTIGO DE REQUISIÇÃO AJAX COM XMLHTTPREQUEST
+
+// const httpRequest = obj => {
+//     const xhr = new XMLHttpRequest();
+
+//     xhr.open(obj.method, obj.url, true);
+//     xhr.send();
+
+//     xhr.addEventListener('load', () => {
+//         if (xhr.status >= 200 && xhr.status < 300) {
+//             obj.success(xhr.responseText);
+//         } else {
+//             obj.error(xhr.statusText);
+//         }
+//     })
+// };
+
+// document.addEventListener('click', e => {
+//     const el = e.target;
+//     const tag = el.tagName.toLowerCase();
+
+//     if (tag === 'a') {
+//         e.preventDefault();
+//         carregaPagina(el);
+//     }
+// })
+
+// function carregaPagina(el) {
+//     const href = el.getAttribute('href');
+
+//     httpRequest({
+//         method: 'GET',
+//         url: href,
+//         success(response) {
+//             carregaResultado(response);
+//         },
+//         error(errorText) {
+//             console.log(errorText);
+//         }
+//     })
+// }
+
+// function carregaResultado(response) {
+//     const resultado = document.querySelector('.resultado');
+//     resultado.innerHTML = response;
+// }
