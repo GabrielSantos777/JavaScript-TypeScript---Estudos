1301a08098a3c428f24bd5d2626244a3b0df5e75
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-03-31 14:26:55 -0300

    como fazer requisições usando o axios

---
+++ secao_08_JS_Assincrono/a06_fetchapi_axios/assets/js/main.js
@@ -0,0 +1,29 @@
+fetch('pessoas.json')
+  .then(response => response.json())
+  .then(data => {
+    carregaElemento(data);
+  });
+
+function carregaElemento(data) {
+  const table = document.createElement('table');
+  for (let pessoa of data) {
+    const tr = document.createElement('tr');
+
+    let td1 = document.createElement('td');
+    td1.innerHTML = pessoa.nome;
+    tr.appendChild(td1);
+
+    let td2 = document.createElement('td');
+    td2.innerHTML = pessoa.idade;
+    tr.appendChild(td2);
+
+    let td3 = document.createElement('td');
+    td3.innerHTML = pessoa.salario;
+    tr.appendChild(td3);
+
+    table.appendChild(tr);
+
+  }
+  const resultado = document.querySelector('.resultado');
+  resultado.appendChild(table);
+}
