790c53e82a81586903f5a880ba0c48d076079937
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-04-08 12:13:49 -0300

    criação e configuração do projeto

--- secao_16_React_Router_Dom_v6/react_router_dom/src/main.tsx
+++ secao_16_React_Router_Dom_v6/react_router_dom/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './components/styles/global.css'
+import { Home } from './components/home/Index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>,
 )
