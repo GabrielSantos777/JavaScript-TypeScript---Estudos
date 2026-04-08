6ef7bed2c61a9012967c12b99c64dcc91b240f2b
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-04-08 12:26:23 -0300

    criação das primeiras rotas e a navegação entre menu usando o Link

---
+++ secao_16_React_Router_Dom_v6/react_router_dom/src/components/Menu/Index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import "./Index.css";
+
+export const Menu = () => {
+  return (
+    <nav className="Menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
--- secao_16_React_Router_Dom_v6/react_router_dom/src/main.tsx
+++ secao_16_React_Router_Dom_v6/react_router_dom/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './components/styles/global.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './components/home/Index.tsx'
+import { About } from './components/About/Index.tsx'
+import { Menu } from './components/Menu/Index.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+    <Menu></Menu>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
