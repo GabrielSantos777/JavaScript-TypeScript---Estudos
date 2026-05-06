65ec35073287cd4289eb4d48a6e30c9e76d40a14
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-05-06 19:37:13 -0300

    chore(aula11_typescript): Aprender exemplos de uso de tipos alias em TypeScript

---
+++ secao_18_Typescript/src/a11_type_alias/a11.ts
@@ -0,0 +1,26 @@
+// TYPE ALIAS
+
+type Idade = number;
+type CorRGB = 'Vermelho' | 'Verde' | 'Azul';
+type CorCMYK = 'Ciano' | 'Marrom' | 'Amarelo';
+type CorPreferida = CorRGB | CorCMYK;
+type Pessoa = {
+  nome: string;
+  idade: Idade;
+  salario: number;
+  corPreferida?: CorPreferida;
+};
+
+const pessoa1: Pessoa = {
+    nome: 'João',
+    idade: 30,
+    salario: 5000,
+    corPreferida: 'Vermelho'
+};
+
+export function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+    return { ... pessoa, corPreferida: cor }
+}
+
+console.log(setCorPreferida(pessoa1, 'Ciano'));
+console.log(pessoa1);
