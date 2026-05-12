971867b74ca76ccdccf5da7d987615c9d83efb9a
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-05-11 22:36:39 -0300

    build(webpack): adiciona config local para aula a16

---
+++ secao_18_Typescript/src/a16_webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("node:path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            configFile: path.resolve(__dirname, "tsconfig.webpack.json"),
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../../dist/assets/js"),
+  },
+  devtool: "source-map",
+};
