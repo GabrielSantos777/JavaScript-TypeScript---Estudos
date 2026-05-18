20ebbed1fbc405fd594e574fe3c81f22e2493c1c
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-05-18 07:51:09 -0300

    feat: adiciona classes Empresa e Colaborador com métodos para gerenciamento de colaboradores

---
+++ secao_18_Typescript/src/a18_classes_typescript/a19.ts
@@ -0,0 +1,30 @@
+// CLASSE
+
+export class Empresa {
+    public readonly nome:string;
+    private readonly colaboradores: Array<Colaborador> = [];
+    protected readonly cnpj:string;
+    constructor(nome:string, cnpj:string){
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    addColaborador(colaborador:Colaborador):void {
+        this.colaboradores.push(colaborador);
+    }
+}
+
+export class Colaborador {
+    constructor(
+        public readonly nome:string,
+        public readonly sobrenome:string
+    ){}
+}
+
+
+const Empresa1 = new Empresa('Facebook', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Luiz', 'Otávio');
+const colaborador2 = new Colaborador('Maria', 'José');
+
+console.log(Empresa1);
+console.log(Empresa1.nome);
